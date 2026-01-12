c3b77366d24eeecfe43c63c6cd4163de781b116b
Apollonic <apollo.nicolas00@gmail.com>
2026-01-12 20:11:11 -0300

    entidade user e resources

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,75 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private Integer ID;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Integer ID, String name, String email, String phone, String password) {
+        this.ID = ID;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(ID, user.ID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(ID);
+    }
+}
---
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    //a anotação @ serve para mapear que esse metodo é a corresponder ao get
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1,"maria","Maroaemail","phone","23q94");
+        return ResponseEntity.ok().body(u);
+    }
+}
