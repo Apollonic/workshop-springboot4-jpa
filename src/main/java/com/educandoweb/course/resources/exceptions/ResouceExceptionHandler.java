0a8e3471061a546a08a9df5b81f6b78e103ab0ee
Apollonic <apollo.nicolas00@gmail.com>
2026-01-21 21:08:10 -0300

    exception delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResouceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResouceExceptionHandler.java
@@ -1,26 +1,34 @@
 package com.educandoweb.course.resources.exceptions;
 
-import com.educandoweb.course.services.exceptions.ResouceNotFoundException;
+import com.educandoweb.course.services.exceptions.DataBaseException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ResouceExceptionHandler {
 
     //essa classe serve para atribuir os valores aos atributos da classe StandardError. Ela prepara o objeto!
 
-    @ExceptionHandler(ResouceNotFoundException.class)
-    public ResponseEntity<StandardError> resourceNotFound (ResouceNotFoundException e, HttpServletRequest request){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound (ResourceNotFoundException e, HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 
-
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBaseException (DataBaseException e, HttpServletRequest request){
+        String error = "DataBase error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.educandoweb.course.services;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
-import com.educandoweb.course.services.exceptions.ResouceNotFoundException;
+import com.educandoweb.course.services.exceptions.DataBaseException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,7 @@ public class UserService {
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
         //implementa a exceção criada
-        return obj.orElseThrow(()-> new ResouceNotFoundException(id));
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User user){
@@ -36,7 +39,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }try {
+            repository.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(long id, User obj){
